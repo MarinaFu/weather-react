2e68e0095fa31ebc54d2257b019ea318ce086111
MarinaFu <133999176+MarinaFu@users.noreply.github.com>
2023-07-26 10:57:40 +0200

    created search.js. filled in styles.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import "./styles.css";
 import React from "react";
-import Weather from "./Weather";
+import Weather from "./weather.js";
 
 export default function App() {
   return <Weather />;
