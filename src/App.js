206988b93b5e9f6d941785ef21f967696f7b5426
MarinaFu <133999176+MarinaFu@users.noreply.github.com>
2023-07-26 10:47:01 +0200

    renaming css file and changing app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
 import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./styles.css";
+import React from "react";
+import Weather from "./Weather";
 
-export default App;
+export default function App() {
+  return <Weather />;
+}
