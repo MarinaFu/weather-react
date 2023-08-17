e61f906aa8d55b98723bc1fd06163bf473bb4656
MarinaFu <133999176+MarinaFu@users.noreply.github.com>
2023-08-17 15:24:13 +0200

    created Forecast.js and css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Search from "./Search";
 import Temperature from "./Temperature";
+import Forecast from "./Forecast";
 import "./App.css";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <div className="App">
       <Search />
       <Temperature />
+      <Forecast />
     </div>
   );
 }
---
+++ src/Forecast.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Forecast.css";
+
+export default function Forecast() {
+  return (
+    <div className="Forecast">
+      <h2>Forecast is</h2>
+    </div>
+  );
+}
