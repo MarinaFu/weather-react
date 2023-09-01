17bf0afc22f85263d21117c98fccfd378bd33beb
MarinaFu <133999176+MarinaFu@users.noreply.github.com>
2023-09-01 19:52:44 +0200

    created useEffect to update forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,7 +18,7 @@ export default function WeatherInfo(props) {
           <div className="forecastImage">
             <WeatherIcon code={props.data.icon} size={62} />{" "}
             <span className="temp">{Math.round(props.data.temperature)}</span>{" "}
-            <span className="units"> ° C</span>
+            <span className="units"> °C</span>
           </div>
         </div>
         <div className="col-6">
